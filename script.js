c6056da40c23e72fd7c6962229be520a62cf6465
SBuchanan1 <syd.buch@gmail.com>
2020-09-19 15:05:04 -0400

    syntax change

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
 });
 
-var queryURL=
+var queryURL =
 
 
-$("#searchBtn").on("click", function () {
-    var apiKey = "#";
-    var searchInput = $("searchBox").val();
-    var queryURL = "#";
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      // console.log(response);
-      $("#currentPage").text("Search:");
-      $("media1").innerHtml();
-      $("media2").innerHtml();
-      $("media3").innerHtml();
-      $("title1").text("Title: " + response);
-      $("title2").text("Title: " + response);
-      $("title3").text("Title: " + response);
-      $("description1").text("Description: " + response);
-      $("description").text("Description: " + response);
-      $("description3").text("Description: " + response);
+    $("#searchBtn").on("click", function () {
+        var apiKey = "#";
+        var searchInput = $("searchBox").val();
+        var queryURL = "#";
 
-    })
-});
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            // console.log(response);
+            $("#currentPage").text("Search:");
+            $("media1").innerHtml();
+            $("media2").innerHtml();
+            $("media3").innerHtml();
+            $("title1").text("Title: " + response);
+            $("title2").text("Title: " + response);
+            $("title3").text("Title: " + response);
+            $("description1").text("Description: " + response);
+            $("description2").text("Description: " + response);
+            $("description3").text("Description: " + response);
+
+        })
+    });
 
 $("#musicBtn").on("click", function () {
     var apiKey = "#";
     var searchInput = $("#searchInput").val();
     var queryURL = ;
-  
+
     $.ajax({
-      url: queryURL,
-      method: "GET",
+        url: queryURL,
+        method: "GET",
     }).then(function (response) {
-      // console.log(response);
-      $("#currentPage").text("Music")
-      $("media1").innerHtml();
-      $("media2").innerHtml();
-      $("media3").innerHtml();
-      $("title1").text("Title: " + response);
-      $("title2").text("Title: " + response);
-      $("title3").text("Title: " + response);
-      $("description1").text("Description: " + response);
-      $("description").text("Description: " + response);
-      $("description3").text("Description: " + response);
-      
+        // console.log(response);
+        $("#currentPage").text("Music")
+        $("media1").innerHtml();
+        $("media2").innerHtml();
+        $("media3").innerHtml();
+        $("title1").text("Title: " + response);
+        $("title2").text("Title: " + response);
+        $("title3").text("Title: " + response);
+        $("description1").text("Description: " + response);
+        $("description2").text("Description: " + response);
+        $("description3").text("Description: " + response);
+
     })
 });
 
@@ -57,22 +57,22 @@ $("#vidBtn").on("click", function () {
     var apiKey = "#";
     var searchInput = $("#searchInput").val();
     var queryURL = ;
-  
+
     $.ajax({
-      url: queryURL,
-      method: "GET",
+        url: queryURL,
+        method: "GET",
     }).then(function (response) {
-      // console.log(response);
-      $("#currentPage").text("Videos")
-      $("media1").innerHtml();
-      $("media2").innerHtml();
-      $("media3").innerHtml();
-      $("title1").text("Title: " + response);
-      $("title2").text("Title: " + response);
-      $("title3").text("Title: " + response);
-      $("description1").text("Description: " + response);
-      $("description").text("Description: " + response);
-      $("description3").text("Description: " + response);
+        // console.log(response);
+        $("#currentPage").text("Videos")
+        $("media1").innerHtml();
+        $("media2").innerHtml();
+        $("media3").innerHtml();
+        $("title1").text("Title: " + response);
+        $("title2").text("Title: " + response);
+        $("title3").text("Title: " + response);
+        $("description1").text("Description: " + response);
+        $("description2").text("Description: " + response);
+        $("description3").text("Description: " + response);
     })
 })
 
@@ -80,21 +80,21 @@ $("#picBtn").on("click", function () {
     var apiKey = "#";
     var searchInput = $("#searchInput").val();
     var queryURL = ;
-  
+
     $.ajax({
-      url: queryURL,
-      method: "GET",
+        url: queryURL,
+        method: "GET",
     }).then(function (response) {
-      // console.log(response);
-      $("#currentPage").text("Jokes")
-      $("media1").innerHtml();
-      $("media2").innerHtml();
-      $("media3").innerHtml();
-      $("title1").text("Title: " + response);
-      $("title2").text("Title: " + response);
-      $("title3").text("Title: " + response);
-      $("description1").text("Description: " + response);
-      $("description").text("Description: " + response);
-      $("description3").text("Description: " + response);
+        // console.log(response);
+        $("#currentPage").text("Jokes")
+        $("media1").innerHtml();
+        $("media2").innerHtml();
+        $("media3").innerHtml();
+        $("title1").text("Title: " + response);
+        $("title2").text("Title: " + response);
+        $("title3").text("Title: " + response);
+        $("description1").text("Description: " + response);
+        $("description2").text("Description: " + response);
+        $("description3").text("Description: " + response);
     })
 })
