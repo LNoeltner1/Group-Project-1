2803c696624b8173353cc38c80c7f8f1823634c5
Lauren Noeltner <laurennoeltner@gmail.com>
2020-09-19 14:30:33 -0400

    pushing JS update

--- script.js
+++ script.js
@@ -1,100 +1,127 @@
-$(document).ready(function() {
-    
+// To create a popup, add the data-role="popup" attribute to a div with
+// the popup contents. Then create a link with the href set to the id of
+// the popup div, and add the attribute data-rel="popup" to tell the framework
+// to open the popup when the link is tapped.
+$(document).ready(function () {
+  $("#permissionPopup").modal("show");
 });
 
-var queryURL=
-
+$("#logo").on("click", function () {
+  $("#currentPage").text("Home Page");
+});
 
 $("#searchBtn").on("click", function () {
-    var apiKey = "#";
-    var searchInput = $("searchBox").val();
-    var queryURL = "#";
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      // console.log(response);
-      $("#currentPage").text("Search:");
-      $("media1").innerHtml();
-      $("media2").innerHtml();
-      $("media3").innerHtml();
-      $("title1").text("Title: " + response);
-      $("title2").text("Title: " + response);
-      $("title3").text("Title: " + response);
-      $("description1").text("Description: " + response);
-      $("description").text("Description: " + response);
-      $("description3").text("Description: " + response);
+  var apiKey = "#";
+  var searchInput = $("searchBox").val();
+  var queryURL = "#";
+  $("#currentPage").text("Search:");
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // console.log(response);
 
-    })
+    $("media1").innerHtml();
+    $("media2").innerHtml();
+    $("media3").innerHtml();
+    $("title1").text("Title: " + response);
+    $("title2").text("Title: " + response);
+    $("title3").text("Title: " + response);
+    $("description1").text("Description: " + response);
+    $("description").text("Description: " + response);
+    $("description3").text("Description: " + response);
+  });
 });
 
 $("#musicBtn").on("click", function () {
-    var apiKey = "#";
-    var searchInput = $("#searchInput").val();
-    var queryURL = ;
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      // console.log(response);
-      $("#currentPage").text("Music")
-      $("media1").innerHtml();
-      $("media2").innerHtml();
-      $("media3").innerHtml();
-      $("title1").text("Title: " + response);
-      $("title2").text("Title: " + response);
-      $("title3").text("Title: " + response);
-      $("description1").text("Description: " + response);
-      $("description").text("Description: " + response);
-      $("description3").text("Description: " + response);
-      
-    })
+  var apiKey = "#";
+  var searchInput = $("#searchInput").val();
+  var queryURL = "";
+  $("#currentPage").text("Music");
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // console.log(response);
+
+    $("media1").innerHtml();
+    $("media2").innerHtml();
+    $("media3").innerHtml();
+    $("title1").text("Title: " + response);
+    $("title2").text("Title: " + response);
+    $("title3").text("Title: " + response);
+    $("description1").text("Description: " + response);
+    $("description").text("Description: " + response);
+    $("description3").text("Description: " + response);
+  });
 });
 
 $("#vidBtn").on("click", function () {
-    var apiKey = "#";
-    var searchInput = $("#searchInput").val();
-    var queryURL = ;
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      // console.log(response);
-      $("#currentPage").text("Videos")
-      $("media1").innerHtml();
-      $("media2").innerHtml();
-      $("media3").innerHtml();
-      $("title1").text("Title: " + response);
-      $("title2").text("Title: " + response);
-      $("title3").text("Title: " + response);
-      $("description1").text("Description: " + response);
-      $("description").text("Description: " + response);
-      $("description3").text("Description: " + response);
-    })
-})
+  var apiKey = "#";
+  var searchInput = $("#searchInput").val();
+  var queryURL = "";
+  $("#currentPage").text("Videos");
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // console.log(response);
+
+    $("media1").innerHtml();
+    $("media2").innerHtml();
+    $("media3").innerHtml();
+    $("title1").text("Title: " + response);
+    $("title2").text("Title: " + response);
+    $("title3").text("Title: " + response);
+    $("description1").text("Description: " + response);
+    $("description").text("Description: " + response);
+    $("description3").text("Description: " + response);
+  });
+});
 
 $("#picBtn").on("click", function () {
-    var apiKey = "#";
-    var searchInput = $("#searchInput").val();
-    var queryURL = ;
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      // console.log(response);
-      $("#currentPage").text("Jokes")
-      $("media1").innerHtml();
-      $("media2").innerHtml();
-      $("media3").innerHtml();
-      $("title1").text("Title: " + response);
-      $("title2").text("Title: " + response);
-      $("title3").text("Title: " + response);
-      $("description1").text("Description: " + response);
-      $("description").text("Description: " + response);
-      $("description3").text("Description: " + response);
-    })
-})
+  var apiKey = "#";
+  var searchInput = $("#searchInput").val();
+  var queryURL = "";
+  $("#currentPage").text("Pictures");
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // console.log(response);
+
+    $("media1").innerHtml();
+    $("media2").innerHtml();
+    $("media3").innerHtml();
+    $("title1").text("Title: " + response);
+    $("title2").text("Title: " + response);
+    $("title3").text("Title: " + response);
+    $("description1").text("Description: " + response);
+    $("description").text("Description: " + response);
+    $("description3").text("Description: " + response);
+  });
+});
+
+$("#artBtn").on("click", function () {
+  var apiKey = "#";
+  var searchInput = $("#searchInput").val();
+  var queryURL =
+    "https://collectionapi.metmuseum.org/public/collection/v1/objects";
+  $("#currentPage").text("Ninja Turtle Art");
+  $.ajax({
+    url: queryURL + "/?t=" + searchInput,
+    method: "GET",
+  }).then(function (response) {
+    // console.log(response);
+
+    $("media1").innerHtml();
+    $("media2").innerHtml();
+    $("media3").innerHtml();
+    $("title1").text("Title: " + response);
+    $("title2").text("Title: " + response);
+    $("title3").text("Title: " + response);
+    $("description1").text("Description: " + response);
+    $("description").text("Description: " + response);
+    $("description3").text("Description: " + response);
+  });
+});
