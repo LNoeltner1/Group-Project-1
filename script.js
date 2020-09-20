33e9826499afbdb52e0004f467671ad56585ecb4
SBuchanan1 <syd.buch@gmail.com>
2020-09-19 23:47:36 -0400

    script stash

--- script.js
+++ script.js
@@ -32,25 +32,25 @@ var queryURL =
 $("#musicBtn").on("click", function () {
     var apiKey = "#";
     var searchInput = $("#searchInput").val();
-    var queryURL = ;
+    var queryURL =
 
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        // console.log(response);
-        $("#currentPage").text("Music")
-        $("media1").innerHtml();
-        $("media2").innerHtml();
-        $("media3").innerHtml();
-        $("title1").text("Title: " + response);
-        $("title2").text("Title: " + response);
-        $("title3").text("Title: " + response);
-        $("description1").text("Description: " + response);
-        $("description2").text("Description: " + response);
-        $("description3").text("Description: " + response);
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            // console.log(response);
+            $("#currentPage").text("Music")
+            $("media1").innerHtml();
+            $("media2").innerHtml();
+            $("media3").innerHtml();
+            $("title1").text("Title: " + response);
+            $("title2").text("Title: " + response);
+            $("title3").text("Title: " + response);
+            $("description1").text("Description: " + response);
+            $("description2").text("Description: " + response);
+            $("description3").text("Description: " + response);
 
-    })
+        })
 });
 
 $("#vidBtn").on("click", function () {
